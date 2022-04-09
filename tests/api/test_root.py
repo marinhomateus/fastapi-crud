4da3cd712729552d7d03d888818b33c1018d1362
Mateus Marinho <mateus.marinho@hotmail.com.br>
2022-04-09 01:21:58 -0300

    tests

---
+++ tests/api/test_root.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+
+
+def test_get_root(client: TestClient) -> None:
+    response = client.get("/")
+    body = response.json()
+    assert response.status_code == 200
+    assert body["msg"] == "ok"
